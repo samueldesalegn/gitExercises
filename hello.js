2a86c2bdcac129040f8a89d3b9aa334a4b48191f
samueldesalegn <samueldesalegnt@gmail.com>
2023-05-06 21:44:06 -0500

    adding hello.js

---
+++ hello.js
@@ -0,0 +1,21 @@
+/**
+ * Change default VS code As the git editor
+ * 
+ * git config --global core.editor "code --wait"
+ * 
+ * git main components 
+ * 1. working area
+ * 
+ * use git add filename
+ * 2. staging area
+ * 
+ * use:
+ * git commit -m "message"
+ * 3. local repository
+ * 
+ * use: git push
+ * 
+ * 4. remote repository such as Github
+ * 
+ */
+
